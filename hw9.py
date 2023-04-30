2349dee3d55b15db181ca296f894073522b76425
IlofMaks <121128704+IlofMaks@users.noreply.github.com>
2023-04-30 18:19:26 +0300

    Update hw9.py

--- hw9.py
+++ hw9.py
@@ -1,9 +1,11 @@
-USERS = {}
+from typing import Dict
+
+USERS: Dict[str,str] = {}
 EXIT_FLAG = False
 
 # decorator
 def error_handler(func):
-    def inner(*args):
+    def inner(args):
         try:
             result = func(*args)
             return result
@@ -15,37 +17,34 @@ def error_handler(func):
             return 'Enter user name'
     return inner
 
-def hello_user(_):
+def hello_user(_) -> str:
     return "How can I help you?"
 
 
-def unknown_command(_):
+def unknown_command(_) -> str:
     return "unknown_command"
 
-def exit(_):
+def exit(_) -> None:
     global EXIT_FLAG
     EXIT_FLAG = True
 
 @error_handler
-def add_user(args):
-    name, phone = args
+def add_user(name, phone):
     USERS[name] = phone
     return f'User {name} added!'
 
 @error_handler
-def change_phone(args):
-    name, phone = args
+def change_phone(name: str, phone: str) -> str:
     USERS[name] = phone
     return f'У {name} тепер телефон: {phone}'
 
-def show_all(_):
+def show_all(_) -> str:
     result = ''
     for name, phone in USERS.items():
         result += f'Name: {name} phone: {phone}\n'
     return result
 
-def show_phone(args):
-    name = args[0]
+def show_phone(name:str) -> str:
     phone = USERS.get(name)
     if phone is None:
         return "User not found"
@@ -63,31 +62,49 @@ HANDLERS = {
 }
 
 def parse_input(user_input):
-    command, *args = user_input.split()
-    command = command.lstrip()
+    args = user_input.split()
+    command = args[0].lower()
 
-    try:
-        handler = HANDLERS[command.lower()]
-    except KeyError:
-        if args:
-            command = command + ' ' + args[0]
-            args = args[1:]
-        handler = HANDLERS.get(command.lower(), unknown_command)
-    return handler, args
+    if command == 'add':
+        if len(args) != 3:
+            return unknown_command, []
+        name, phone = args[1:]
+        return add_user, [name, phone]
+
+    if command == 'change':
+        if len(args) != 3:
+            return unknown_command, []
+        name, phone = args[1:]
+        return change_phone, [name, phone]
+
+    if command == 'phone':
+        if len(args) != 2:
+            return unknown_command, []
+        name = args[1]
+        return show_phone, name
+
+    if command == 'show' and len(args) == 2 and args[1] == 'all':
+        return show_all, []
+
+    if command in ('hello', 'hi'):
+        return hello_user, []
+
+    if command in ('exit', 'quit', 'goodbye', 'close'):
+        return exit, []
+
+    return unknown_command, []
 
 
 def main():
     while not EXIT_FLAG:
         # example: add Petro 0991234567
         user_input = input('Please enter command and args: ')
-        handler, *args = parse_input(user_input)
-        result = handler(*args)
-        result = handler(*args)
+        handler, args = parse_input(user_input)
+        result = handler(args)
         if not result:
-            print('Exit')
+            print('Good Bye')
             break
         print(result)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
